d205cbebca549ffa9b5684caa83346db25f851fe
José Anchieta <santosjr87@gmail.com>
2024-03-07 00:16:42 -0300

    Improve titles and flash messages

--- ios/sporticalapp/Endpoint.swift
+++ ios/sporticalapp/Endpoint.swift
@@ -3,9 +3,9 @@ import Foundation
 enum Endpoint {
     static var rootURL: URL {
 //        #if DEBUG
-//        return URL(string: "http://localhost:3000")!
+        return URL(string: "http://localhost:3000")!
 //        #else
-        return URL(string: "https://www.sportical.app")!
+//        return URL(string: "https://www.sportical.app")!
 //        #endif
     }
 
