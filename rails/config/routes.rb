6b6e828e58fbd3243a13214c8b4db7820ce3614f
José Anchieta <santosjr87@gmail.com>
2024-03-12 09:40:21 -0300

    Add event and calendar example before user sign up or sign in

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,18 +1,24 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  # SESSIONS
   get 'sessions/new', as: :login
   post 'sessions/create', as: :signin
   delete 'sessions/destroy', to: 'sessions#destroy', as: :signout
+
+  # CUSTOM PAGES
   get 'pages/get_started', as: :get_started
   get 'pages/support', as: :support
   get 'pages/policy', as: :policy
+  get 'pages/event_example', as: :event_example
+
+  # RESOURCES
   resources :events
   resources :users, except: %i[index show]
-
   resources :configurations, only: [] do
     get :ios, on: :collection
   end
 
+  # ROOT
   root 'events#index'
 end
